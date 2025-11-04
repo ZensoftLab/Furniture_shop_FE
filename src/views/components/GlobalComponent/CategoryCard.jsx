c1041ca8afceffb1775cc7a615709d4694fc32df
emonislive <emon9580@gmail.com>
2025-11-04 17:26:32 +0600

    fix: broken Route Link in Category Card

--- src/views/components/GlobalComponent/CategoryCard.jsx
+++ src/views/components/GlobalComponent/CategoryCard.jsx
@@ -1,19 +1,24 @@
+import { Link } from "react-router-dom";
+
 function CategoryCard({ category }) {
     return (
-        <div
+        // ! switching from <a> to <Link> for client-side routing 
+        <Link
+            to={category.link} // * using 'to' prop for Link component
             className="relative flex items-center justify-center w-full h-full bg-cover bg-center rounded-lg shadow-lg hover:scale-105 transition-transform"
             style={{
                 backgroundImage: `url(${category.imageUrl})`,
                 backgroundSize: 'cover',
             }}
         >
+
             <a
-                href={category.link}
+                // href={category.link} // * removed for Link component
                 className="absolute inset-0 flex items-end pb-6 justify-center bg-opacity-50 text-white text-2xl font-semibold rounded-lg hover:bg-opacity-70"
             >
                 <p className="bg-black/50 px-5 rounded-full p-2 ">{category.name}</p>
             </a>
-        </div>
+        </Link>
     );
 }
 
